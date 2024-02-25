97f2a3fda6290826e3066c8cd7a6991fd254ff5f
kristijan.potocnik <kristijan456@gmail.com>
2024-02-25 11:20:34 +0000

    first commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var py = "pkgagain"
+const pz = "pkgand again"
+
+
+func main() {
+	x := "test"
+	var y = "again"
+	const z = "and again"
+	fmt.Println(x, y, z, py, pz)
+}
